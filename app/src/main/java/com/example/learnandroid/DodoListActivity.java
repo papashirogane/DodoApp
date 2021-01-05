78b1ec5a932d117e0a93610984668e2a91e32485
papashirogane <papashirogane@gmail.com>
2021-01-05 13:43:02 -0800

    Added temporary toolbar to DodoListActivity. Added clickedDodo to DodoWaddle

--- app/src/main/java/com/example/learnandroid/DodoListActivity.java
+++ app/src/main/java/com/example/learnandroid/DodoListActivity.java
@@ -3,6 +3,7 @@ package com.example.learnandroid;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
@@ -17,17 +18,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.learnandroid.model.dodo.Dodo;
+import com.example.learnandroid.model.dodo.DodoWaddle;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DodoListActivity extends AppCompatActivity {
-    private List<Dodo> waddle = new ArrayList<>();
+    private DodoWaddle MyWaddle = DodoWaddle.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodo_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarDodoList);
+        setSupportActionBar(toolbar);
         
         populateWaddle();
         populateListView();
@@ -39,8 +43,8 @@ public class DodoListActivity extends AppCompatActivity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Dodo clickedDodo = waddle.get(i);
-                Toast.makeText(DodoListActivity.this, "You clicked on " + clickedDodo.getName(), Toast.LENGTH_SHORT).show();
+                MyWaddle.clickedDodo = MyWaddle.get(i);
+                Toast.makeText(DodoListActivity.this, "You clicked on " + MyWaddle.clickedDodo.getName(), Toast.LENGTH_SHORT).show();
                 /*Intent intent = ThirdActivity.makeIntent(DodoListActivity.this);
                 startActivity(intent);*/
                 // opens new activity which has fill-in-the-blanks for sailor health, sailor distance, and dodo distance in metres
@@ -51,12 +55,12 @@ public class DodoListActivity extends AppCompatActivity {
     }
 
     private void populateWaddle() {
-        waddle.add(new Dodo("Baby", 3, R.drawable.lazy1, "Waaaaa!"));
-        waddle.add(new Dodo("Emo Teen", 5, R.drawable.lazy2, "Go away mom."));
-        waddle.add(new Dodo("Mama Dodo", 7, R.drawable.lazy3, "I gotta quit smoking."));
-        waddle.add(new Dodo("Happy Papi", 10, R.drawable.lazy4, "Borgor."));
-        waddle.add(new Dodo("Big Uncle", 15, R.drawable.lazy5, "Look at these GUNS *slaps biceps*"));
-        waddle.add(new Dodo("Lil Rhea", 20, R.drawable.lazy6, "Uuuhhhhh..."));
+        MyWaddle.add(new Dodo("Baby", 3, R.drawable.lazy1, "Waaaaa!"));
+        MyWaddle.add(new Dodo("Emo Teen", 5, R.drawable.lazy2, "Go away mom."));
+        MyWaddle.add(new Dodo("Mama Dodo", 7, R.drawable.lazy3, "I gotta quit smoking."));
+        MyWaddle.add(new Dodo("Happy Papi", 10, R.drawable.lazy4, "Borgor."));
+        MyWaddle.add(new Dodo("Big Uncle", 15, R.drawable.lazy5, "Look at these GUNS *slaps biceps*"));
+        MyWaddle.add(new Dodo("Lil Rhea", 20, R.drawable.lazy6, "Uuuhhhhh..."));
     }
 
     private void populateListView() {
@@ -72,7 +76,7 @@ public class DodoListActivity extends AppCompatActivity {
 
     private class DodoAdapter extends ArrayAdapter<Dodo> {
         public DodoAdapter() {
-            super(DodoListActivity.this, R.layout.item_view, waddle);
+            super(DodoListActivity.this, R.layout.item_view, MyWaddle.waddle);
         }
 
         @NonNull
@@ -83,7 +87,7 @@ public class DodoListActivity extends AppCompatActivity {
                 itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
             }
             // Now we have a view, but it's just using the defaults I put in item_view.xml. I want to put in my own icon, name, weight, and height.
-            Dodo currentDodo = waddle.get(position);
+            Dodo currentDodo = MyWaddle.get(position);
 
             ImageView imageView = (ImageView) itemView.findViewById(R.id.item_icon); // not sure why we have to findViewById inside of ImageView instead of just this activity
             imageView.setImageResource(currentDodo.getIconId());
--- app/src/main/java/com/example/learnandroid/model/dodo/DodoWaddle.java
+++ app/src/main/java/com/example/learnandroid/model/dodo/DodoWaddle.java
@@ -10,7 +10,9 @@ import java.util.List;
  * List of dodos (a group of dodos is called a "waddle").
  */
 public class DodoWaddle implements Iterable<Dodo> {
-    private List<Dodo> waddle = new ArrayList<>();
+    private static DodoWaddle single_instance = null;
+    public List<Dodo> waddle = new ArrayList<>();
+    public Dodo clickedDodo;
 
     private DodoWaddle() {
         waddle.add(new Dodo("Baby", 3, R.drawable.lazy1, "Waaaaa!"));
@@ -18,7 +20,14 @@ public class DodoWaddle implements Iterable<Dodo> {
         waddle.add(new Dodo("Mama Dodo", 7, R.drawable.lazy3, "I gotta quit smoking."));
         waddle.add(new Dodo("Happy Papi", 10, R.drawable.lazy4, "Borgor."));
         waddle.add(new Dodo("Big Uncle", 15, R.drawable.lazy5, "Look at these GUNS *slaps biceps*"));
-        waddle.add(new Dodo("Lil Rhea", 20, R.drawable.lazy6, "Uuuhhhhh..."));
+        waddle.add(new Dodo("Lil Rhea", 20, R.drawable.lazy6, "UwU"));
+    }
+
+    public static DodoWaddle getInstance() {
+        if (single_instance == null) {
+            single_instance = new DodoWaddle();
+        }
+        return single_instance;
     }
 
     public void add(Dodo dodo) {
