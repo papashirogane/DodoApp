2914050ee4c4e2805594b5983c8280a9ea5c95f3
papashirogane <papashirogane@gmail.com>
2021-01-09 20:12:37 -0800

    Implemented icon selection in AddActivity (haven't done to Edit1Activity yet). Icons are currently stand-ins and must be changed later.

--- app/src/main/java/com/example/learnandroid/AddActivity.java
+++ app/src/main/java/com/example/learnandroid/AddActivity.java
@@ -1,12 +1,20 @@
 package com.example.learnandroid;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +26,12 @@ import com.example.learnandroid.model.dodo.DodoWaddle;
  */
 public class AddActivity extends BaseActivity {
     DodoWaddle waddle = DodoWaddle.getInstance();
+    private int clickedIconId = R.drawable.dodo;
+    private int[] images = {R.drawable.ic_add, R.drawable.ic_settings
+            , R.drawable.ic_home, R.drawable.ic_info
+            , R.drawable.ic_night, R.drawable.ic_edit};
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +42,8 @@ public class AddActivity extends BaseActivity {
         getSupportActionBar().setTitle("Create Your Dodo!");
 
         setupHints();
-        setupButton();
+        setupTableButtons();
+        setupCreateButton();
     }
 
     private void setupHints() {
@@ -41,18 +55,75 @@ public class AddActivity extends BaseActivity {
         textView.setHint(R.string.add_hintDetails);
     }
 
-    private void setupButton() {
+    /**
+     * Based on Vinayak Bevinakatti's answer from
+     * https://stackoverflow.com/questions/5327553/android-highlight-an-imagebutton-when-clicked
+     */
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @SuppressLint("ClickableViewAccessibility")
+    private void setupTableButtons() {
+        TableLayout table = findViewById(R.id.input_tableLayout);
+        int rows = table.getChildCount();
+        for (int y = 0; y < rows; y++) {
+            TableRow row = (TableRow) table.getChildAt(y);
+            int cols = row.getChildCount();
+            for (int x = 0; x < cols; x++) {
+                ImageButton button = (ImageButton) row.getChildAt(x);
+                Drawable clickedIcon = getDrawable(images[(y * cols) + x]);
+                button.setImageDrawable(clickedIcon);
+
+                final int finalY = y;
+                final int finalX = x;
+                button.setOnTouchListener((view, motionEvent) -> {
+                    switch (motionEvent.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            // If not pressed, unpress all other buttons
+                            // if pressed, do nothing
+                            if (!view.isPressed()) {
+                                clearPressed();
+                                view.setPressed(true);
+                                view.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                                view.invalidate();
+                            }
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            clickedIconId = images[ finalY *cols + finalX];
+                        default:
+                            break;
+                    }
+                    return true;
+                });
+            }
+        }
+    }
+
+    private void clearPressed() {
+        TableLayout table = findViewById(R.id.input_tableLayout);
+        int rows = table.getChildCount();
+        for (int y = 0; y < rows; y++) {
+            TableRow row = (TableRow) table.getChildAt(y);
+            int cols = row.getChildCount();
+            for (int x = 0; x < cols; x++) {
+                ImageButton button = (ImageButton) row.getChildAt(x);
+                button.setPressed(false);
+                button.getBackground().clearColorFilter();
+                button.invalidate();
+            }
+        }
+    }
+
+    private void setupCreateButton() {
         Button button = (Button) findViewById(R.id.input_btnSubmit);
         button.setText(R.string.add_txtSubmit);
         button.setOnClickListener(view -> {
             String name = ((TextView) findViewById(R.id.input_txtfill1)).getText().toString();
-            String details = ((TextView) findViewById(R.id.input_txtfill3)).getText().toString();
             String tempMass = ((TextView) findViewById(R.id.input_txtfill2)).getText().toString();
             Double mass = Double.parseDouble(tempMass);
+            String details = ((TextView) findViewById(R.id.input_txtfill3)).getText().toString();
 
             if (name == "") Toast.makeText(AddActivity.this, "Name cannot be empty. Try again.", Toast.LENGTH_LONG).show();
             else {
-                Dodo newDodo = new Dodo(name, mass, R.drawable.lazy1, details);
+                Dodo newDodo = new Dodo(name, mass, clickedIconId, details);
                 waddle.add(newDodo);
                 Toast.makeText(AddActivity.this, "You created a dodo!", Toast.LENGTH_LONG).show();
                 Intent intent = DodoListActivity.makeIntent(AddActivity.this);
