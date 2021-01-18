9c1f63ee3042cfdc2d7e320247fb3b282b83ebe3
papashirogane <papashirogane@gmail.com>
2021-01-17 18:15:01 -0800

    Changed icon options to be proper dodo icons.

--- app/src/main/java/com/example/learnandroid/model/dodo/DodoWaddle.java
+++ app/src/main/java/com/example/learnandroid/model/dodo/DodoWaddle.java
@@ -8,21 +8,23 @@ import java.util.List;
 
 /**
  * List of dodos (a group of dodos is called a "waddle").
+ * Keeps track of which dodo was most recently clicked.
+ * Includes list of icon options (for adding or editing dodos).
  */
 public class DodoWaddle implements Iterable<Dodo> {
     private static DodoWaddle single_instance = null;
     public List<Dodo> waddle = new ArrayList<>();
     public Dodo clickedDodo;
-    public final int[] iconOpts = {R.drawable.ic_add, R.drawable.ic_settings
-            , R.drawable.ic_home, R.drawable.ic_info
-            , R.drawable.ic_night, R.drawable.ic_edit};
+    public final int[] iconOpts = {R.drawable.ic_beak, R.drawable.ic_egg
+            , R.drawable.ic_gizzard, R.drawable.ic_mayor
+            , R.drawable.ic_rhea, R.drawable.ic_wing};
 
     private DodoWaddle() {
-        waddle.add(new Dodo("Baby", 3, R.drawable.lazy1, "One day I'm gonna be 15 metres tall!"));
-        waddle.add(new Dodo("Gizzard", 7, R.drawable.lazy3, "Gizzard stones play an important role in my species' digestion."));
-        waddle.add(new Dodo("Mayor", 10, R.drawable.lazy4, "Humans call me a mayor to insinuate that I'm rich and fat."));
-        waddle.add(new Dodo("Chomper", 15, R.drawable.lazy5, "I use my strong beak to crush raw fruit... and to bite sailors."));
-        waddle.add(new Dodo("Lil Rhea", 20, R.drawable.lazy6, "I'm about the same mass as a small Rhea americana."));
+        waddle.add(new Dodo("Baby", 3, R.drawable.ic_egg, "One day I'm gonna be 15 metres tall!"));
+        waddle.add(new Dodo("Gizzard", 7, R.drawable.ic_gizzard, "Gizzard stones play an important role in my species' digestion."));
+        waddle.add(new Dodo("Mayor", 10, R.drawable.ic_wing, "Humans call me a mayor to insinuate that I'm rich and fat."));
+        waddle.add(new Dodo("Chomper", 15, R.drawable.ic_beak, "I use my strong beak to crush raw fruit... and to bite sailors."));
+        waddle.add(new Dodo("Lil Rhea", 20, R.drawable.ic_rhea, "I'm about the same mass as a small Rhea americana."));
     }
 
     public static DodoWaddle getInstance() {
